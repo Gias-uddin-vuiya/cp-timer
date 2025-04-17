937e1d402d575207419657df08b24c799a66aeea
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-04-17 09:40:21 +0600

    added script file and switch full screen funcationality to script file

---
+++ frontend/js/script.js
@@ -0,0 +1,11 @@
+
+const fullscreenButton = document.getElementById("fullscreenToggle");
+
+
+fullscreenButton.addEventListener("click", () => {
+  if (!document.fullscreenElement) {
+    document.documentElement.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+});
