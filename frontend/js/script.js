6c77da3315b5fd137ee5405cbdd8b14392606bc1
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-04-17 10:13:32 +0600

    added 15 minutes time for testing popurse

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,30 @@
 
+const displayTime = document.querySelector(".display-time")
+const startTimer = document.querySelector(".start-btn")
 const fullscreenButton = document.getElementById("fullscreenToggle");
+
+
+let countdown;
+let timeLeft = 15 * 60; // min in second
+
+startTimer.addEventListener('click', (evt) => {
+    evt.preventDefault()
+    
+    clearInterval(countdown)
+
+    countdown = setInterval(() => {
+        let minute = Math.floor(timeLeft / 60)
+        let second = timeLeft % 60;
+
+        displayTime.textContent = `${String(minute).padStart(2, "0")}:${String(second).padStart(2, "0")}`
+
+        if (timeLeft < 0) {
+            clearInterval(countdown)
+        }
+
+        timeLeft--
+    }, 1000)
+})
 
 
 fullscreenButton.addEventListener("click", () => {
