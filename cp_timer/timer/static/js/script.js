de3036419aa5ec829ce1d10f0ec6e15b06faa4a3
Gias-uddin-vuiya <giasuddinvuiya2022@gmail.com>
2025-05-24 12:01:17 +0600

    feat: add modal for selecting competitive practice mode

--- cp_timer/timer/static/js/script.js
+++ cp_timer/timer/static/js/script.js
@@ -1,5 +1,11 @@
+const body = document.querySelector('body')
 const fullscreenButton = document.getElementById("fullscreenToggle");
+const cpMode = document.querySelector(".challenge-msg")
 
+
+cpMode.addEventListener('click', () => {
+  
+})
 
 fullscreenButton.addEventListener("click", () => {
   if (!document.fullscreenElement) {
